cf9f5b98fba782df3c7d7ccd749ef3ee2b491028
Sreang Rathanak <sreang.rathanak.framgia.com>
2015-06-18 00:04:55 +0700

    Styleing Admin V2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
       "#{base_title} | #{title}"
     end
   end
+
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+ 
+  def action?(*action)
+     action.include?(params[:action])
+  end
 end
